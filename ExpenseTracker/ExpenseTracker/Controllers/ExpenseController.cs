dd0e8a48ef6a547eb94da63474a1912b41a7d384
Nebojsa <millovac@gmail.com>
2021-08-16 20:17:52 +0200

    Initial commit

---
+++ ExpenseTracker/ExpenseTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpenseController : ControllerBase
+    {
+        [HttpGet("api/expense")]
+        public IActionResult Expense()
+        {
+            return Ok(new { ekspense = "Test"});
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public ExpenseType ExpenseType { get; set; }
+        public Bussines Bussines { get; set; }
+    }
+}
